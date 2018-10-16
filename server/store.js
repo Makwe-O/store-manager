e97e91d5a73d87081d85e9b4b3e6411e9136b6f5
onyeka mmakwe <notmaks@yahoo.com>
2018-10-16 11:53:33 +0100

    Added dummy database [Finishes #161248708]

---
+++ server/store.js
@@ -0,0 +1,16 @@
+export default {
+  products: [
+    {
+      id: 1,
+      name: 'Sony SPS',
+      price: 22000,
+      inventory: 20,
+    },
+    {
+      id: 2,
+      name: 'Sony SPS',
+      price: 22000,
+      inventory: 20,
+    },
+  ],
+};
