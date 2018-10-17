c8e9bbc5a91c697cdaa52baa5a1accdb0cbd2fc5
onyeka mmakwe <notmaks@yahoo.com>
2018-10-17 06:39:19 +0100

    Created specific product endpoint

--- server/controllers/Product.js
+++ server/controllers/Product.js
@@ -5,5 +5,25 @@ const Product = {
     const products = ProductModel;
     return res.send(products);
   },
+  getOne(req, res) {
+    const { id } = req.params;
+    let found = false;
+    const product = ProductModel;
+    product.Products.forEach((item) => {
+      if (item.id === id) {
+        found = true;
+        return res.json(item);
+      }
+    });
+    if (!found) {
+      res.status(404).json('Product not found');
+    }
+
+
+    // if (!product) {
+    //   return res.status(404).send({ message: 'product not found' });
+    // }
+    // return res.send(product);
+  },
 };
 export default Product;
--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -3,5 +3,6 @@ import Product from '../controllers/Product';
 
 const router = express.Router();
 router.get('/', Product.getAll);
+router.get('/:id', Product.getOne);
 
 export default router;
