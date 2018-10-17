e50b1ccb36f2837104bacd4d016739c20d6f0c4b
onyeka mmakwe <notmaks@yahoo.com>
2018-10-17 13:17:56 +0100

    Wrote test cases for getting specific sales record

--- test/sales.spec.js
+++ test/sales.spec.js
@@ -15,3 +15,30 @@ describe('GET /sales', () => {
       });
   });
 });
+
+describe('GET /sales/:id', () => {
+  it('Endpoint should return 404 if an invalid id is passed', (done) => {
+    chai.request(server)
+      .get(`/api/v1/sales/${7}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done(err);
+      });
+  });
+  it('should be an object', (done) => {
+    chai.request(server)
+      .get(`/api/v1/sales/${1}`)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        done(err);
+      });
+  });
+  it('should return a sale record if id is valid', (done) => {
+    chai.request(server)
+      .get(`/api/v1/sales/${1}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done(err);
+      });
+  });
+});
